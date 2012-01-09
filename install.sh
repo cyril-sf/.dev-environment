32f1a63ae54f69da9453a211db476d47b35dbf8b
Cyril Fluck <cyril@fluck.fr>
2012-01-09 15:07:10 -0800

    update installer to create symbolic links for janus .vimrc.before and .vimrc.after

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@ ln -s ~/dev-environment/.bash_profile ~/.bash_profile
 ln -s ~/dev-environment/.gitconfig ~/.gitconfig
 ln -s ~/dev-environment/.gitignore ~/.gitignore
 ln -s ~/dev-environment/.irbrc ~/.irbrc
+ln -s ~/dev-environment/.vimrc.before ~/.vimrc.before
+ln -s ~/dev-environment/.vimrc.after ~/.vimrc.after
