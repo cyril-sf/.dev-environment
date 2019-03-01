ec6bbad59da3c3ce81113d51168ffa8b79032fdf
Cyril Fluck <github@fluck.fr>
2019-03-01 13:27:51 -0800

    Remove old code

--- install.sh
+++
@@ -1,7 +0,0 @@
-ln -s ~/dev-environment/.bash_aliases ~/.bash_aliases
-ln -s ~/dev-environment/.bash_profile ~/.bash_profile
-ln -s ~/dev-environment/.gitconfig ~/.gitconfig
-ln -s ~/dev-environment/.gitignore ~/.gitignore
-ln -s ~/dev-environment/.irbrc ~/.irbrc
-ln -s ~/dev-environment/.vimrc.before ~/.vimrc.before
-ln -s ~/dev-environment/.vimrc.after ~/.vimrc.after
